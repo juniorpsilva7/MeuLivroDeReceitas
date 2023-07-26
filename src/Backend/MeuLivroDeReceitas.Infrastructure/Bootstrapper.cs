f827c85f4b7f5fe8812c4d51ba14bbb1d375a817
Junior <juniorpsilva7@gmail.com>
2023-07-26 20:35:30 -0300

    Classes para acessar o repositoriode usuario

--- src/Backend/MeuLivroDeReceitas.Infrastructure/Bootstrapper.cs
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/Bootstrapper.cs
@@ -1,8 +1,13 @@
 ﻿using FluentMigrator.Runner;
 using MeuLivroDeReceitas.Domain.Extension;
+using MeuLivroDeReceitas.Domain.Repositorios;
+using MeuLivroDeReceitas.Infrastructure.AcessoRepositorio;
+using MeuLivroDeReceitas.Infrastructure.AcessoRepositorio.Repositorio;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Serialization;
 
 namespace MeuLivroDeReceitas.Infrastructure;
 
@@ -11,6 +16,32 @@ public static class Bootstrapper
     public static void AddRepositorio(this IServiceCollection services, IConfiguration configurationManager)
     {
         AddFluentMigrator(services, configurationManager);
+
+        AddContexto(services, configurationManager);
+        AddUnidadeDeTrabalho(services);
+        AddRepositorios(services);
+    }
+
+    private static void AddContexto(IServiceCollection services, IConfiguration configurationManager)
+    {
+        var versaoServidor = new MySqlServerVersion(new Version(8,0,33));
+        var connectionString = configurationManager.GetConexaoCompleta();
+
+        services.AddDbContext<MeuLivroDeReceitasContext>(dbContextoOpcoes =>
+        {
+            dbContextoOpcoes.UseMySql(connectionString, versaoServidor);
+        });
+    }
+
+    private static void AddUnidadeDeTrabalho(IServiceCollection services)
+    {
+        services.AddScoped<IUnidadeDeTrabalho, UnidadeDeTrabalho>();
+    }
+
+    private static void AddRepositorios(IServiceCollection services)
+    {
+        services.AddScoped<IUsuarioWriteOnlyRepositorio, UsuarioRepositorio>()
+            .AddScoped<IUsuarioReadOnlyRepositorio, UsuarioRepositorio>();
     }
 
     private static void AddFluentMigrator(IServiceCollection services, IConfiguration configurationManager)
